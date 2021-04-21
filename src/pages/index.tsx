eb084d2983753d1326b7ab7209e8ee4f5d092646
Vinicius Silva <vinicius_prsilva@hotmail.com>
2021-04-20 21:24:42 -0300

    Estrutura do App

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
-import styles from '../styles/home.module.scss';
+
+import Head from 'next/head';
 
 
 export default function Home() {
   return (
-    <h1 className={styles.tittle}>Hello world!</h1>
+    <>
+    <Head> 
+       <title>Inicio | IgNews</title>
+    </Head>
+    <h1>Hello world!</h1>
+    </>
   )
 }
