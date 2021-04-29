a877bdcaf37d97bd988ef7521751ff67be7f84be
Vinicius Silva <vinicius_prsilva@hotmail.com>
2021-04-29 17:24:56 -0300

    fix: Client's duplication

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,18 +1,60 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import {query as q} from 'faunadb';
+
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
 
+type User = {
+    ref: {
+        id: string,
+    }
+    data: { 
+        stripe_customer_id: string,
+    }
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+
     if(req.method === 'POST') {
 
         const session = await getSession({ req })
 
+        // Selecionar usuário por email
+    const user = await fauna.query<User>(
+        q.Get(
+            q.Match(
+                q.Index('users_by_email'),
+                q.Casefold(session.user.email),
+            )
+        )
+    )
+    
+    // Evitar que o usuário seja duplicado///
+    let customerId = user.data.stripe_customer_id
+    
+    if(!customerId) {
         const stripeCustomer = await stripe.customers.create({
             email: session.user.email, // o E-mail agora é obrigatório
         })
 
+        await fauna.query(
+            q.Update(
+                q.Ref(q.Collection('users'), user.ref.id),
+                {
+                    data: { 
+                        stripe_customer_id: stripeCustomer.id,
+                    }
+                }
+            )
+        )
+        
+        customerId = stripeCustomer.id;
+    }
+
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id, // O id do produto que esta no stripe, e não no faunaDB
+            customer: customerId, // O id do produto que esta no stripe, e não no faunaDB
             payment_method_types: ['card'], // Pagamento no cartão
             billing_address_collection: 'required', // Quero obrigar que o cliente preencha o endereço
             line_items: [ // itens
